42291380998ff3d30ab20199d5be6194fa2af652
michielman2 <michiel.meeuwisse@gmail.com>
2024-10-07 11:27:17 +0200

    new project

---
+++ src/main/java/nl/bioinf/gse/Gene.java
@@ -0,0 +1,7 @@
+package nl.bioinf.gse;
+
+public class Gene {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/nl/bioinf/gse/Main.java
@@ -0,0 +1,8 @@
+package nl.bioinf.gse;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+// hallo bla bla
