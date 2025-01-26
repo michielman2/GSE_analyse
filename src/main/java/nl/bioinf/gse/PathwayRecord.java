19d2e0ace5d0b61988e977fa00e87c2d9993e788
michielman2 <michiel.meeuwisse@gmail.com>
2025-01-26 15:23:35 +0100

    add javadocs to pathwayrecord

--- src/main/java/nl/bioinf/gse/PathwayRecord.java
+++ src/main/java/nl/bioinf/gse/PathwayRecord.java
@@ -2,5 +2,11 @@ package nl.bioinf.gse;
 
 import java.util.List;
 
+/**
+ * A record representing a biological pathway and its associated metadata.
+ *
+ * @param pathwayID  The unique identifier for the pathway.
+ * @param description A textual description of the pathway.
+ * @param geneIDs    A list of gene identifiers associated with the pathway.
+ */
 public record PathwayRecord(String pathwayID, String description, List<String> geneIDs) {}
-
